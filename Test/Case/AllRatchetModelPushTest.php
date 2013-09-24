842525e2b01854a30eeb59f7349fec56e13f6824
wyrihaximus <ceesjank@gmail.com>
2013-09-24 23:42:59 +0200

    Bare plugin generate by @friendsofcake/travis

---
+++ Test/Case/AllRatchetModelPushTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * All RatchetModelPush plugin tests
+ */
+class AllRatchetModelPushTest extends CakeTestCase {
+
+/**
+ * Suite define the tests for this plugin
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('All RatchetModelPush test');
+
+		$path = CakePlugin::path('RatchetModelPush') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+
+		return $suite;
+	}
+
+}
